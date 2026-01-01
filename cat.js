c925eeb62566933fc052cec8c5638e549c74742e
sunerok <justinvforvendetta@gmail.com>
2026-01-01 13:24:53 -0500

    initial commit

---
+++ cat.js
@@ -0,0 +1,66 @@
+
+var scbf = function(d,t){
+    var c = d.createElement(t),
+	i = d.createElement(t),
+	s = d.getElementsByTagName(t)[0],
+	sf = 200,
+	st = 10000,
+	img = '/nyan/img/design/';
+	
+    var up = function()
+    {
+		$(this).animate({
+		    'marginTop' : '-=10'
+		}, sf, i.down);
+    }
+    var down = function()
+	{
+		$(this).animate({
+		    'marginTop' : '+=10'
+		}, sf,i.up );
+    }
+    
+    i.up = up;
+    i.down = down;
+    
+    $(i).addClass('i')
+		.css({
+		'background' : 'url(' + img + 'cat.gif) 50% 0 repeat',
+		'height' : '100%',
+		'width' : '450px', 
+		'position' : 'absolute',
+		'right' : '0'
+    	});
+	
+    i.up();
+	
+    $(c).addClass('c')
+		.hide()
+		.fadeIn(1000)
+		.animate({
+		    'width' : '+=120%'
+		}, st, 'linear', function(){
+			$(this).fadeOut(function(){
+				$(this).remove();
+			});
+		})
+		.css({
+		    'position' : 'absolute',
+		    'left' : '-200px',
+		    'top' : '200px',
+		    'z-index' : '100',
+		    'width' : '350px',
+		    'height' : '350px',
+		    'position' : 'absolute',
+			//'background' : 'url(' + img + 'trail.gif) 100% 0 repeat-x',
+		})
+		.html(i);
+    s.parentNode.insertBefore(c,s);
+
+};
+
+$( document ).ready(function() {
+        console.log( "document loaded" );
+		scbf(document,'div');
+		k = [];
+});
