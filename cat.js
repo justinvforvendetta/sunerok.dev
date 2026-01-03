9f21b71c5e8721ff1d88fa23b9254046653adaec
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2026-01-02 19:58:04 -0500

    Update cat.js

--- cat.js
+++ cat.js
@@ -2,11 +2,10 @@ var scbf = function(d,t){
     var c = d.createElement(t),
         i = d.createElement(t),
         s = d.getElementsByTagName(t)[0],
-        sf = 200,      // vertical bounce speed
-        st = 10000,    // horizontal movement speed
+        sf = 200,
+        st = 10000,
         img = '/nyan/img/design/';
     
-    // Bounce up/down functions
     var up = function() {
         $(this).animate({'marginTop' : '-=10'}, sf, i.down);
     }
@@ -17,19 +16,17 @@ var scbf = function(d,t){
     i.up = up;
     i.down = down;
     
-    // Cat image (50% smaller)
     $(i).addClass('i')
         .css({
             'background' : 'url(' + img + 'cat.gif) 100% 0 no-repeat',
-            'height' : '50%',     // 50% smaller
-            'width' : '225px',    // 50% of 450px
+            'height' : '100%',
+            'width' : '450px', 
             'position' : 'absolute',
             'right' : '0'
         });
     
-    i.up(); // start bounce
+    i.up();
     
-    // Container div (no trail)
     $(c).addClass('c')
         .hide()
         .fadeIn(1000)
@@ -43,12 +40,12 @@ var scbf = function(d,t){
             'left' : '-200px',
             'top' : '200px',
             'z-index' : '100',
-            'width' : '225px',  // match cat width
-            'height' : '175px', // roughly proportional
-            'background' : 'none' // removed trail.gif
+            'width' : '350px',
+            'height' : '350px',
+            'position' : 'absolute',
+            'background' : 'none'   // trail removed
         })
         .html(i);
-    
     s.parentNode.insertBefore(c,s);
 };
 
